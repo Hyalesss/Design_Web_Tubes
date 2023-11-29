93bbd50adbdbad54ce2bf8a4944715418ff8c358
unknown <11211007@student.itk.ac.id>
2023-11-29 22:39:08 +0800

    page berita aman , tinggal fitur search

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,30 +3,58 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div><Container>
-            <Row>
-                <Col>
-                <div>
-                <img src="./gotnews.png" className="pe-2" alt="GOT NEWS Logo" />
-                <h5 className="">GOT NEWS</h5>
-                <p className="desc">gotnews</p>
-                </div>
-                    <p>Text Paragraf</p>
-                    <div>
-                        <Link>
-                            <i className="fa-brands fa-whatsapp"></i>
-                            <p>+62 8950 1192 321</p>
-                        </Link>
-                    </div>
-                    <div>
-                        <Link>
-                            <i className="fa-regular fa-envelope"></i>
-                            <p>Alessandrobenaya5@gmail.com</p>
-                        </Link>
-                    </div>
-                </Col>
-            </Row>
-        </Container></div>
+        <div className="footer py-5">
+            <Container>
+                <Row className="d-flex justify-content-between">
+                    <Col lg="5"> 
+                        <div className="logos">
+                            <div className="text-logos">
+                                <img src="./gotnews.png" className="pe-2" alt="GOT NEWS Logo" />
+                                <h5 className="textlogos">GOT NEWS</h5>
+                            </div>
+                            <p className="desc">Got News membantu anda untuk melihat informasi yang sedemikian rupa dengan kategori yang ada </p>
+                        </div>
+                        <div className="no mb-1 mt-4">
+                            <Link className="text-decoration-none">
+                                <i className="fa-brands fa-whatsapp"></i>
+                                <p className="m-0">+62 8950 1192 321</p>
+                            </Link>
+                        </div>
+                        <div className="mail">
+                            <Link className="text-decoration-none">
+                                <i className="fa-regular fa-envelope"></i>
+                                <p className="m-0">Alessandrobenaya5@gmail.com</p>
+                            </Link>
+                        </div>
+                    </Col>
+                    <Col className="d-flex flex-column col-lg-2 col mt-lg-0 mt-5">
+                        <h5 className="fw-bold">Menu</h5>
+                        <Link to="">Beranda</Link>
+                        <Link to="Berita">Berita</Link>
+                        <Link to="videoberita">Video Berita</Link>
+                        <Link to="FAQ">FAQ</Link>
+                    </Col>
+                    <Col lg="4" className="mt-lg-0 mt-5">
+                        <h5 className="fw-bold mb-3">Subscribe untuk info menarik</h5>
+                        <div className="subscribe">
+                            <input type="text" placeholder="Subscribe..." />
+                            <button className="btn btn-primary rounded-2">Subscribe</button>
+                        </div>
+                        <div className="social mt-3 ">
+                            <i className="fa-brands fa-facebook"></i>
+                            <i className="fa-brands fa-twitter"></i>
+                            <i className="fa-brands fa-linkedin"></i>
+                            <i className="fa-brands fa-youtube"></i>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p className="tekscopyright text-center fw-bold px-md-0 px-3">&copy; Copyright {new Date().getFullYear} by GOT NEWS Portal Berita</p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
 
--- src/pages/Berita.jsx
+++ src/pages/Berita.jsx
@@ -1,8 +1,40 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { semuaberita } from "../data/index"
+
 const Berita = () => {
     return (
-        <div> 
-            <header className="w-100 min-vh-100"></header>
+        <div className="beritapage">
+            <div className="berita-hangat min-vh-100">
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1 className="fw-bold text-center">Semua Berita</h1>
+                            <p className="text-center">Semua berita hangat & lama tersedia di halaman ini</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        {semuaberita.map((hangat) => {
+                            return <Col className="shadow-sm" key={hangat.id}>
+                                <img src={hangat.imagehangat} alt="unsplash.com" className="w-100 mb-5 rounded-top" />
+                                <div className="star mb-2 px-3">
+                                    <i className={hangat.star1}></i>
+                                    <i className={hangat.star2}></i>
+                                    <i className={hangat.star3}></i>
+                                    <i className={hangat.star4}></i>
+                                    <i className={hangat.star5}></i>
+                                </div>
+                                <h5 className="mb-5 px-3">{hangat.title}</h5>
+                                <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
+                                    <p className="m-0 text-secondary fw-bold">{hangat.price}</p>
+                                    <button className="btn btn-primary rounded-1">{hangat.buy}</button>
+                                </div>
+                            </Col>
+                        })}
+                    </Row>
+                </Container>
+            </div>
         </div>
+
     )
 }
 
