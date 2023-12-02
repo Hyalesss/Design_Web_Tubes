88abb84e0722abf51691f0cd54d1d849b1d6a722
siredenta <89696526+xentauzzyam@users.noreply.github.com>
2023-12-02 08:44:43 +0800

    video beritaa

---
+++ src/data/indexvideo.js
@@ -0,0 +1,64 @@
+import BeritaVideo1 from '../assets/img/beritaVideo/video1.jpg';
+import BeritaVideo2 from '../assets/img/beritaVideo/video2.jpg';
+import BeritaVideo3 from '../assets/img/beritaVideo/video3.jpg';
+import BeritaVideo4 from '../assets/img/beritaVideo/video4.jpg';
+
+export const videoData = [
+    {
+      id: 1,
+      image: BeritaVideo1,
+      star1: "fa-solid fa-star",
+      star2: "fa-solid fa-star",
+      star3: "fa-solid fa-star",
+      star4: "fa-solid fa-star",
+      star5: "fa-solid fa-star-half-stroke",
+      title: 'Status Gunung Anak Krakatau Siaga Level Tiga',
+      url: 'https://www.youtube.com/watch?v=jwT6_Wo83Z0',
+      price: "Putri Siregar | November 2023",
+      buy: "Tonton Video",
+      delay: "",
+    },
+    {
+        id: 2,
+        image: BeritaVideo2,
+        star1: "fa-solid fa-star",
+        star2: "fa-solid fa-star",
+        star3: "fa-solid fa-star",
+        star4: "fa-solid fa-star",
+        star5: "fa-solid fa-star",
+        title: 'Netizen Indonesia Gempur Psikologis Tentara Israel',
+        url: 'https://www.youtube.com/watch?v=a3f3appESaw',
+        price: "Cynthia Putri Siregar | November 2023",
+        buy: "Tonton Video",
+        delay: "200 ",
+      },
+      {
+        id: 3,
+        image: BeritaVideo3,
+        star1: "fa-solid fa-star",
+        star2: "fa-solid fa-star",
+        star3: "fa-solid fa-star",
+        star4: "fa-solid fa-star",
+        star5: "fa-solid fa-star",
+        title: 'Tentara Israel Kena Mental Dirujak Netizen Indonesia, Sosmed Anggota Zionis Jadi Target Hujatan',
+        url: 'https://www.youtube.com/watch?v=glSzyAvATno',
+        price: "Cynthia Putri Siregar | Oktober 2023",
+        buy: "Tonton Video",
+        delay: "400",
+      },
+      {
+        id: 4,
+        image: BeritaVideo4,
+        star1: "fa-solid fa-star",
+        star2: "fa-solid fa-star",
+        star3: "fa-solid fa-star",
+        star4: "fa-solid fa-star",
+        star5: "fa-solid fa-star",
+        title: 'Luhut Dijenguk Puan di Singapura, Bahas Pemilu 2024',
+        url: 'https://www.youtube.com/watch?v=8mfpLv4I02c',
+        price: "Cynthia Putri Siregar | Oktober 2023",
+        buy: "Tonton Video",
+        delay: "",
+      },
+    // Add more video data as needed
+  ];
--- src/pages/Berita.jsx
+++ src/pages/Berita.jsx
@@ -1,8 +1,71 @@
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { semuaberita } from "../data/index"
+
 const Berita = () => {
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredBerita = semuaberita.filter((hangat) =>
+        hangat.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
-        <div> 
-            <header className="w-100 min-vh-100"></header>
+        <div className="beritapage">
+            <div className="berita-hangat min-vh-100">
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1 className="fw-bold text-center">Semua Berita</h1>
+                            <p className="text-center">Semua berita hangat & lama tersedia di halaman ini</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <input
+                                type="text"
+                                placeholder="Cari berita..."
+                                value={searchTerm}
+                                onChange={handleSearch}
+                                className="form-control mb-4"
+                            />
+                            {filteredBerita.length > 0 ? (
+                                filteredBerita.map((hangat) => (
+                                    <Col className="shadow-sm" key={hangat.id}>
+                                    </Col>
+                                ))
+                            ) : (
+                                <p className="text-center">Tidak ada berita yang ditemukan</p>
+                            )}
+                        </Col>
+                    </Row> 
+                    <Row>
+                        {filteredBerita.map((hangat) => {
+                            return <Col className="shadow-sm" key={hangat.id}>
+                                <img src={hangat.imagehangat} alt="unsplash.com" className="w-100 mb-5 rounded-top" />
+                                <div className="star mb-2 px-3">
+                                    <i className={hangat.star1}></i>
+                                    <i className={hangat.star2}></i>
+                                    <i className={hangat.star3}></i>
+                                    <i className={hangat.star4}></i>
+                                    <i className={hangat.star5}></i>
+                                </div>
+                                <h5 className="mb-5 px-3">{hangat.title}</h5>
+                                <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
+                                    <p className="m-0 text-secondary fw-bold">{hangat.price}</p>
+                                    <button className="btn btn-primary rounded-1">{hangat.buy}</button>
+                                </div>
+                            </Col>
+                        })}
+                    </Row>
+                </Container>
+            </div>
         </div>
+
     )
 }
 
